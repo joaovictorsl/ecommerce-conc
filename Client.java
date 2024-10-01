802a30131c24b759d9f9f1ba9f62784f2898e70a
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2024-10-01 14:48:41 -0300

    Add: Client

--- Client.java
+++ Client.java
@@ -1,3 +1,24 @@
-public class Client {
-    
+import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+
+public class Client implements Runnable {
+    private BlockingQueue<Order> orderQueue;
+
+    public Client(BlockingQueue<Order> q) {
+        orderQueue = q;
+    }
+
+    @Override
+    public void run() {
+        try {
+            orderQueue.put(newOrder());
+        } catch (Exception e) {
+        }
+    }
+
+    private Order newOrder() {
+        Order o = new Order(UUID.randomUUID());
+        o.addItem("Something", 10);
+        return o;
+    }
 }
