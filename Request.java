0f46438bbaa7cf5a722882174d0de24e91ce2129
losout0 <leandro.oliveira.souto@ccc.ufcg.edu.br>
2024-10-01 14:34:34 -0300

    Request

--- Request.java
+++ Request.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Request {
-    
+    private UUID id;
+    private ArrayList<String> itens;
+
+    public Request(UUID id) {
+        this.id = id;
+        itens = new ArrayList<String>();
+    }
+
+    public void addItem(String item) {
+        itens.add(item);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
 }
