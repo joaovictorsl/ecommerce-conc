4071d33dee6cb58abb4f3a68b695a40ed2e77ade
raniel dourado <raniel.dourado@lsd.ufcg.edu.br>
2024-10-03 17:37:18 -0300

    add: item to model

---
+++ src/model/Item.java
@@ -0,0 +1,40 @@
+package src.model;
+
+import java.util.UUID;
+
+public class Item {
+    private String id;
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
