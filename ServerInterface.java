18cd32ca430465dcc845506ac464323f6faef37f
MayaBrito <mayara.pinheiro@ccc.ufcg.edu.br>
2024-10-03 10:31:25 -0300

    Organizing the folder directory

--- ServerInterface.java
+++
@@ -1,9 +0,0 @@
-public interface ServerInterface {
-
-    public void addItemToStorage(String item, Integer quantity);
-
-    public void removeItemFromStorage(String item, Integer quantity);
-
-    public boolean verifyItemStorage(String item);
-
-}
