36cc6e31e66e0f83c552648f8a499b0cc9002c25
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2024-10-03 20:47:21 -0300

    Add: client

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,24 +1,44 @@
+package src.model;
+
+import java.util.List;
+import java.util.Random;
 import java.util.UUID;
-import java.util.concurrent.BlockingQueue;
+
+import src.service.Server;
 
 public class Client implements Runnable {
-    private BlockingQueue<Order> orderQueue;
+    private Server server;
+    private List<Item> availableItems;
+    private Random random = new Random();
+    private String clientId;
 
-    public Client(BlockingQueue<Order> q) {
-        orderQueue = q;
+    public Client(Server server, List<Item> items) {
+        this.server = server;
+        this.availableItems = items;
+        this.clientId = "Cliente-" + UUID.randomUUID();
     }
 
     @Override
     public void run() {
         try {
-            orderQueue.put(newOrder());
+            Order order = newOrder();
+            server.addOrderToQueue(order);
+            System.out.println(clientId + " adicionou um novo pedido.");
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     private Order newOrder() {
-        Order o = new Order(UUID.randomUUID());
-        o.addItem("Something", 10);
-        return o;
+        Order order = new Order();
+        int numItemsToOrder = random.nextInt(availableItems.size()) + 1;
+
+        for (int i = 0; i < numItemsToOrder; i++) {
+            Item item = availableItems.get(random.nextInt(availableItems.size()));
+            int quantity = random.nextInt(10) + 1;
+            order.addItem(item, quantity);
+        }
+
+        return order;
     }
 }
